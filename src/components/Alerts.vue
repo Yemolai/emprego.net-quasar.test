e12859384c4785221a15e2784daf201962f8b0ad
Gabriel Rodrigues <yemolai@gmail.com>
2019-12-13 12:43:47 -0300

    [Component] composing Alert layout

--- src/components/Alerts.vue
+++ src/components/Alerts.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="alerts-wrapper">
+  <div class="alerts-wrapper text-white">
     <template v-for="(alert, key) in list">
       <q-banner inline-actions rounded :class="bannerClass(alert)" :key="key">
-        {{ alert.message }}
+        <span class="text-h4 q-my-sm">
+          {{ alert.icon }}
+        </span>
+        <span class="text-h5">{{ alert.message }}</span>
         <template v-slot:action>
-          <q-btn outline rounded :label="alert.action" />
+          <q-btn outline rounded :label="alert.actionLabel" />
         </template>
       </q-banner>
     </template>
@@ -18,6 +21,18 @@ export default {
     list: {
       type: Array,
       default: () => ([])
+    }
+  },
+  methods: {
+    bannerClass (alert) {
+      const color = alert.type === 'invite' ? 'warning' : 'primary'
+      const textColor = alert.type === 'invite' ? 'dark' : 'white'
+      return [
+        'q-ma-md',
+        'q-pa-md',
+        `bg-${color}`,
+        `text-${textColor}`
+      ]
     }
   }
 }

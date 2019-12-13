c8cb683ae5314154783100bb48f93968852d1b72
Gabriel Rodrigues <yemolai@gmail.com>
2019-12-13 13:10:31 -0300

    [Component] CV notification

---
+++ src/components/CurriculumAlert.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="curriculum-alert bg-accent q-pa-lg q-ma-md text-white">
+    <div class="close-button ripple">×</div>
+    <p>Você ainda não preencheu o currículo.</p>
+    <p>As empresas não vão te encontrar assim.</p>
+    <q-btn push class="text-lowercase q-px-lg" color="primary">
+      completar currículo
+    </q-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CurriculumAlert'
+}
+</script>
+
+<style lang="scss" scoped>
+.curriculum-alert {
+  border-radius: 15px;
+  position: relative;
+  p {
+    margin-top: 2px;
+    margin-bottom: 2px;
+  }
+  &>.close-button {
+    position: absolute;
+    top: 1px;
+    right: 12px;
+    font-size: 28px;
+    cursor: pointer;
+  }
+}
+</style>
